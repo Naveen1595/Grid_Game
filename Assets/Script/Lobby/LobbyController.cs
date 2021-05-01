5239ebabc170559e78783d3ebe07700804d61e55
BoardGame <naveen.rtw1595@gmail.com>
2021-05-01 18:48:41 +0530

    Player Movement added

---
+++ Assets/Script/Lobby/LobbyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    [SerializeField]private Button startButton;
+    [SerializeField]private Button ExitButton;
+
+    private void Update()
+    {
+        startButton.onClick.AddListener(StartTheGame);
+        ExitButton.onClick.AddListener(ExitTheGame);
+    }
+
+    void StartTheGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    void ExitTheGame()
+    {
+        Application.Quit();
+    }
+
+}
